19924c690f4a092f6c42b656b0d7a0a7122a4b02
Bharath Upadhya <1992bharath@gmail.com>
2017-09-06 16:53:00 +0530

    Update sempaphore.py

--- sempaphore.py
+++ sempaphore.py
@@ -3,20 +3,20 @@ import time
 sem = threading.Semaphore()
 
 def fun1():
-    for num in range(1,200):
+    for num in range(1,20):
         sem.acquire()
         print(1)
         sem.release()
         time.sleep(0.25)
 
 def fun2():
-    for num in range(1,200):
+    for num in range(1,20):
         sem.acquire()
         print(2)
         sem.release()
         time.sleep(0.25)
 
-#t = threading.Thread(target = fun1)
-#t.start()
-#t2 = threading.Thread(target = fun2)
-#t2.start()
+t = threading.Thread(target = fun1)
+t.start()
+t2 = threading.Thread(target = fun2)
+t2.start()
